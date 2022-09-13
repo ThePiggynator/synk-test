9f1c44d3e8ccbb586aad960998ef2633ff36cd27
ThePiggynator <hugoblok1313@gmail.com>
2022-09-13 10:35:31 +0200

    Ok

--- connectfour/src/App31.vue
+++ connectfour/src/App31.vue
@@ -1,16 +1,19 @@
 <template>
   <HeaderComponent/>
+  <NavbarComponent/>
   <WelcomeComponent/>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue'
 import WelcomeComponent from './components/WelcomeComponent.vue'
+import NavbarComponent from './components/NavbarComponent.vue'
 
 export default {
   name: 'AppThirtyOne',
   components: {
     HeaderComponent,
+    NavbarComponent,
     WelcomeComponent
   }
 }
