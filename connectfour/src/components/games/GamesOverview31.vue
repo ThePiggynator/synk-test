4079df207fa6c9b226f4f8f3ed2f67bf5b1440f8
ThePiggynator <hugoblok1313@gmail.com>
2022-09-22 10:48:13 +0200

    3.2.1 Af
    Based

--- connectfour/src/components/games/GamesOverview31.vue
+++ connectfour/src/components/games/GamesOverview31.vue
@@ -1,41 +1,45 @@
 <template>
-    <table>
+  <table>
+    <tr>
+      <th>ID:</th>
+      <th>Title:</th>
+      <th>Created at:</th>
+      <th>Status:</th>
+      <th>Rated:</th>
+      <th>Max think Time:</th>
+    </tr>
+    <tbody>
+    <template v-for="data in gameData" :key='data'>
       <tr>
-        <th>ID:</th>
-        <th>Title:</th>
-        <th>Created at:</th>
-        <th>Status:</th>
-        <th>Rated:</th>
-        <th>Max think Time:</th>
+        <th>{{ data.getId() }}</th>
+        <th>{{ data.getTitle() }}</th>
+        <th>{{ data.getCreatedAt() }}</th>
+        <th>{{ data.getStatus() }}</th>
+        <th>{{ data.getRated() }}</th>
+        <th>{{ data.getMaxThinkTime() }}</th>
       </tr>
-      <tbody>
-      <template v-for="data in gameData" :key='data'>
-        <tr>
-          <th>{{data.getId()}}</th>
-          <th>{{data.getTitle()}}</th>
-          <th>{{data.getCreatedAt()}}</th>
-          <th>{{data.getStatus()}}</th>
-          <th>{{data.getRated()}}</th>
-          <th>{{data.getMaxThinkTime()}}</th>
-        </tr>
-      </template>
-      </tbody>
-
-    </table>
+    </template>
+    </tbody>
+  </table>
+  <button class="button" v-on:click="onNewGame">New game</button>
 </template>
 
 <script>
-import { Game } from '@/models/Game.js';
-
+import {Game} from '@/models/Game.js';
+let lastId = 30000;
 export default {
   name: "GamesOverview31",
   created() {
-    let lastId = 30000;
-
     for (let i = 0; i < 8; i++) {
       lastId += Math.ceil(Math.random() * 3)
       this.gameData.push(Game.createSampleGame(lastId))
     }
+  },
+  methods: {
+    onNewGame() {
+      lastId += Math.ceil(Math.random() * 3)
+      this.gameData.push(Game.createSampleGame(lastId))
+    },
   },
   data() {
     return {
@@ -47,12 +51,12 @@ export default {
 </script>
 
 <style scoped>
-  table{
-    border: 1px solid #000;
-    width: 100%;
-  }
-  th{
-    border-left: 1px solid #000;
-    border-right: 1px solid #000;
-  }
+table {
+  border: 1px solid #000;
+  width: 100%;
+}
+
+th {
+  border: 1px solid #000;
+}
 </style>
