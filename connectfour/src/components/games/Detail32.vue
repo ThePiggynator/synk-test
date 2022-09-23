a57e4de5a414d9c446789259eea0c9f06c5971da
ThePiggynator <hugoblok1313@gmail.com>
2022-09-23 15:52:31 +0200

    3.2.2 B

--- connectfour/src/components/games/Detail32.vue
+++ connectfour/src/components/games/Detail32.vue
@@ -1,15 +1,49 @@
-<!--eslint-disable-->
 <template>
-
+  <div class="gameContainer">
+    <table>
+      <tr>
+        <th>Game Details</th>
+        <th>(Id: )</th>
+      </tr>
+      <tr>
+        <th>Title:</th>
+        <th><input type="text"></th>
+      </tr>
+      <tr>
+        <th>Status:</th>
+        <th><input type="text"></th>
+      </tr>
+      <tr>
+        <th>Rated:</th>
+        <th><input type="text"></th>
+      </tr>
+      <tr>
+        <th>Max Thinking Time:</th>
+        <th><input type="text"></th>
+      </tr>
+      <tr>
+        <th>Created at:</th>
+        <th><input type="date"></th>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
+
+import {Game} from "@/models/Game";
+
 export default {
   /* eslint-disable */
-  name: "Detail32"
+  name: "Detail32",
+  props: {
+    selectedGame: Game
+  }
 }
 </script>
 
 <style scoped>
-
+.gameContainer {
+  border: 1px solid #000;
+}
 </style>
--- connectfour/src/components/games/GamesOverview32.vue
+++ connectfour/src/components/games/GamesOverview32.vue
@@ -1,27 +1,37 @@
 <template>
-  <table>
-    <tr>
-      <th>ID:</th>
-      <th>Title:</th>
-    </tr>
-    <tbody>
-    <template v-for="data in gameData" :key='data'>
-      <tr v-on:click="selectGame(data.getId())" :id="data.getId()">
-        <th>{{ data.getId() }}</th>
-        <th>{{ data.getTitle() }}</th>
-      </tr>
+  <div class="container">
+    <div class="table">
+      <table>
+        <tr>
+          <th>ID:</th>
+          <th>Title:</th>
+        </tr>
+        <tbody>
+        <template v-for="data in gameData" :key='data'>
+          <tr v-on:click="selectGame(data.getId())" :id="data.getId()">
+            <th>{{ data.getId() }}</th>
+            <th>{{ data.getTitle() }}</th>
+          </tr>
+        </template>
+        </tbody>
+      </table>
+      <button class="button" v-on:click="onNewGame">New game</button>
+    </div>
+    <template v-if="selectedGameId !== 0">
+      <detail32 selected-game="{{this.selectedGame}}"/>
     </template>
-    </tbody>
-  </table>
-  <button class="button" v-on:click="onNewGame">New game</button>
-
+    <template v-else>
+      <h2>Select a game</h2>
+    </template>
+  </div>
 </template>
 
 <script>
+import Detail32 from './Detail32.vue'
 import {Game} from '@/models/Game.js';
+
 let lastId = 30000;
 // eslint-disable-next-line no-unused-vars
-let selectedGame = null
 export default {
   name: "GamesOverview32",
 
@@ -36,36 +46,62 @@ export default {
       lastId += Math.ceil(Math.random() * 3)
       this.gameData.push(Game.createSampleGame(lastId))
     },
-    selectGame(gameId){
-      if (gameId != selectedGame){
+    selectGame(gameId) {
+      if (gameId !== this.selectedGameId) {
         document.getElementById(gameId).style.background = "#42b983";
-        if (selectedGame != null){
-          document.getElementById(selectedGame).style.background = "#ffffff";
+        if (this.hasGameSelected()) {
+          document.getElementById(this.selectedGameId).style.background = "#ffffff";
         }
-        selectedGame = gameId;
+        this.selectedGameId = gameId;
+        this.findGame(gameId)
       } else {
-        console.log(selectedGame);
-        document.getElementById(selectedGame).style.background = "#ffffff";
-        selectedGame = null;
+        document.getElementById(this.selectedGameId).style.background = "#ffffff";
+        this.selectedGameId = 0;
+        this.selectedGame = null;
+      }
+    },
+    hasGameSelected() {
+      return this.selectedGameId !== 0
+    },
+    findGame(id) {
+      for (let i = 0; i < this.gameData.length; i++) {
+        if (this.gameData[i].getId() === id) {
+          this.selectedGame = this.gameData[i]
+          console.log(typeof(this.selectedGame))
+        }
       }
     }
   },
   data() {
     return {
+      selectedGameId: 0,
       gameData: [],
+      selectedGame: null
     };
   },
+  components: {
+    Detail32
+  }
 }
 </script>
 
 <style scoped>
+.table {
+  width: 40%;
+}
+
 table {
   border: 1px solid #000;
-  width: 33%;
+  width: 100%;
 
 }
 
 th {
   border: 1px solid #000;
+}
+
+.container {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
